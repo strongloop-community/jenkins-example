ea3ebf24931066b25a07efc1fe75246ddf3e2d73
Chema <chemitaxis@gmail.com>
2017-08-14 13:33:05 +0200

    Update new method for binding

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var toAdd = 23
 function summer (sumVal) {
   return function (toAdd) {
     if (!toAdd) throw new Error('trying to pull a fast one?')
-    return sumVal + toAdd
+    return sumVal + toAdd - 1
   }
 }
 
